1838e6bc6ea1e89538bb6b627db5c65fa6e7c8eb
Maxwell L <mleuthner2002@gmail.com>
2023-10-03 15:05:53 -0500

    install

--- client/src-sw.js
+++ client/src-sw.js
@@ -54,7 +54,7 @@ registerRoute(
       }),
       new ExpirationPlugin({
         maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
   })
